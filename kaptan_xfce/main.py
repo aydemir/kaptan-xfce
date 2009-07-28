a9a6c571a916b689e43f12273b5ec07621f27804
Gökmen Görgen <gkmngrgn@gmail.com>
2009-07-28 23:48:43 +0300

    add content and vertical menu. but don't forget, vertical menu won't be label.

--- kaptan_xfce/main.py
+++ kaptan_xfce/main.py
@@ -9,26 +9,47 @@ import os
 import gtk
 
 from aboutdialog import AboutDialog
+from common import get_file
 
 class MainWindow:
     def __init__(self):
         self.builder = gtk.Builder()
-        self.builder.add_from_file('kaptan_xfce/ui/kaptan-xfce.glade')
+        self.builder.add_from_file(get_file('kaptan-xfce.glade'))
         self.builder.connect_signals({
-            'on_about_button_clicked': self.aboutdialog,
+            'on_about_button_clicked': self.aboutDialog,
             'gtk_main_quit': gtk.main_quit,
             })
 
-        self.mainWindow = self.builder.get_object('mainWindow')
-        self.mainWindow.fullscreen()
+        self.main_window = self.builder.get_object('main_window')
+        self.main_window.fullscreen()
 
-        self.mainWindow.show_all()
+        self.content(self.builder)
 
-    def aboutdialog(self, widget = None):
+        self.main_window.show_all()
+
+    def aboutDialog(self, widget = None):
         AboutDialog()
 
     def quit(self):
         gtk.main_quit()
+
+    def content(self, builder):
+        hbox = builder.get_object('content_hbox')
+
+        content_label = gtk.Label('Content Label')
+        self.verticalMenu(builder, hbox)
+
+        hbox.pack_end(content_label)
+
+    def verticalMenu(self, builder, hbox):
+        menu_items = ["<big><b>Welcome!</b></big>", "Mouse", "Themes", "Menu", \
+                      "Wallpaper", "Package", "Summary", "Good Bye!"]
+
+        vertical_menu_label = gtk.Label()
+        vertical_menu_label.set_markup('» %s' % '\n'.join(menu_items))
+
+        hbox.pack_start(vertical_menu_label, expand = 0,
+                        fill = 0, padding = 5)
 
     def main(self):
         gtk.main()
