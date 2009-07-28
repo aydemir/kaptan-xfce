e0cf7171cf9d2db932d13309bb0c028b7acb579c
Gökmen Görgen <gkmngrgn@gmail.com>
2009-07-28 23:47:05 +0300

    add website and logos.

--- kaptan_xfce/aboutdialog.py
+++ kaptan_xfce/aboutdialog.py
@@ -7,8 +7,11 @@
 
 import gtk
 
-from common import SUMMARY, APPNAME, AUTHORS, TRANSLATORS, VERSION, \
-                    LICENSE, COPYRIGHT, WEBSITE
+# FIX ME: Url show does not work.
+#~ from gnome import url_show
+from common import get_file
+from common import SUMMARY, APPNAME, AUTHORS, TRANSLATOR_CREDITS, \
+                    VERSION, LICENSE, COPYRIGHT, WEBSITE
 
 class AboutDialog(gtk.AboutDialog):
     def __init__(self):
@@ -17,13 +20,15 @@ class AboutDialog(gtk.AboutDialog):
         self.set_comments(SUMMARY)
         self.set_name(APPNAME)
         self.set_authors(AUTHORS.split('\n'))
-        #~ self.set_translator_credits(TRANSLATORS.split('\n'))
+        self.set_translator_credits(TRANSLATOR_CREDITS)
         self.set_version(VERSION)
         self.set_license(LICENSE)
+        #~ gtk.about_dialog_set_url_hook(lambda self, url: url_show(url))
         self.set_website(WEBSITE)
+        self.set_website_label(WEBSITE)
         self.set_copyright(COPYRIGHT)
-#~ self.set_logo(gtk.gdk.pixbuf_new_from_file(get_pixmap("kaptan.png")))
-#~ self.set_icon(gtk.gdk.pixbuf_new_from_file(get_pixmap("kaptan.png")))
+        self.set_logo(gtk.gdk.pixbuf_new_from_file(get_file("kaptan.png")))
+        self.set_icon(gtk.gdk.pixbuf_new_from_file(get_file("kaptan-logo.png")))
 
         self.show_all()
         self.run()
